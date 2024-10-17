815c2d712ff99d4957f80f901b2227e5f00d58c0
Daniel Cox <danielcox16@hotmail.com>
2024-10-17 16:13:11 +0200

    Break up original script and add learn_lut, update other functions

--- online_slm_calibration/calibration_functions.py
+++ online_slm_calibration/calibration_functions.py
@@ -1,8 +1,10 @@
 # External 3rd party
+import torch
 from torch import Tensor as tt
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 # External ours
 from openwfs.algorithms.troubleshoot import field_correlation
@@ -26,16 +28,36 @@ def phase_response(input_phase, c, dim=-3):
     return actual_phase
 
 
-def predict_feedback(phase_in1, phase_in2, a, b, c, N, noise_level):
+def predict_feedback(gray_value0, gray_value1, a: tt, b: tt, phase_response_per_gv: tt, nonlinearity,
+                     noise_level=0.0) -> tt:
     """
 
     Args:
+        gray_value0: Tensor containing gray values of group 0, corresponding to dim 0 of feedback.
+        gray_value1: Tensor containing gray values of group 1, corresponding to dim 1 of feedback.
+        a: Predicted feedback offset.
+        b: Predicted feedback interference signal factor.
+        phase_response_per_gv: Phase response per gray value. The corresponding gray values coincide with
+            the array index.
+        nonlinearity: Nonlinearity number. 1 = linear, 2 = 2PEF, 3 = 3PEF, etc., 0 = detector is broken :)
+        noise_level: Optional noise level.
 
+    Returns:
+        Predicted
     """
-    phase_diff_actual = phase_response(phase_in2, c) - phase_response(phase_in1, c)
-    feedback_clean = a + b * (torch.cos(phase_diff_actual / 2)**(2*N))
-    feedback = feedback_clean + noise_level * torch.randn(feedback_clean.shape)
-    return feedback
+    # Set array dimensions
+    gv0 = gray_value0.view(-1, 1)
+    gv1 = gray_value1.view(1, -1)
+
+    # Compute phase difference and predicted clean feedback
+    phase_diff_actual = phase_response(gv1, phase_response_per_gv) - phase_response(gv0, phase_response_per_gv)
+    feedback_clean = a + b * (torch.cos(phase_diff_actual / 2) ** (2 * nonlinearity))
+
+    # Add optional noise if requested
+    if noise_level == 0.0:
+        return feedback_clean
+    else:
+        return feedback_clean + noise_level * torch.randn(feedback_clean.shape)
 
 
 def plot_phase_curve(c_pred, phase, phase_lut_correct):
@@ -54,9 +76,9 @@ def plot_phase_curve(c_pred, phase, phase_lut_correct):
     plt.ylim((-2*np.pi, 4*np.pi))
 
 
-def plot_feedback_fit(feedback_meas, feedback, phase1, phase2):
+def plot_feedback_fit(feedback_meas, feedback, gray_values0, gray_values1):
     plt.subplot(1, 3, 2)
-    extent = (phase2.min(), phase2.max(), phase1.min(), phase1.max())
+    extent = (gray_values1.min(), gray_values1.max(), gray_values0.min(), gray_values0.max())
     vmin = feedback_meas.min()
     vmax = feedback_meas.max()
     plt.imshow(feedback_meas.squeeze().detach(), extent=extent, interpolation='nearest', vmin=vmin, vmax=vmax)
@@ -85,3 +107,65 @@ def import_lut(filepath_lut, scaling=8.0) -> tt:
     return torch.tensor(numbers) / scaling
 
 
+def learn_lut(gray_values0: tt, gray_values1: tt, feedback_measurements: tt, nonlinearity=2, iterations: int = 1000,
+              init_noise_level=0.1, do_plot: bool = False, plot_per_its: int = 10, do_end_plot: bool = True) -> tt:
+    """
+    Learn the phase lookup table from dual phase stepping measurements.
+
+    Args:
+        gray_values0:
+        gray_values1: Same as gray_values1, for the second group.
+        feedback_measurements:
+        nonlinearity: Nonlinearity number. 1 = linear, 2 = 2PEF, 3 = 3PEF, etc., 0 = detector is broken :)
+        iterations: Number of learning iterations.
+        do_plot: If True, plot during learning.
+        plot_per_its: Plot per this many learning iterations.
+        init_noise_level: Standard deviation of the Gaussian noise added to the initial phase_response guess.
+    Returns:
+    """
+    # Create init prediction
+    phase_response_per_gv = torch.linspace(0, 2*np.pi, 256) + torch.randn(256) * init_noise_level
+    phase_response_per_gv.requires_grad = True
+    a = torch.tensor(feedback_measurements.mean(), requires_grad=True)
+    b = torch.tensor(3*feedback_measurements.std(), requires_grad=True)
+
+
+    # Initialize parameters and optimizer
+    learning_rate = 5e-2
+    params = [{'lr': learning_rate, 'params': [phase_response_per_gv]}]
+    optimizer = torch.optim.Adam(params, lr=learning_rate, amsgrad=True)
+
+    progress_bar = tqdm(total=iterations)
+
+    if do_plot:
+        plt.figure(figsize=(13, 4))
+
+    for it in range(iterations):
+        feedback_predicted = predict_feedback(gray_values0, gray_values1, phase_response_per_gv, nonlinearity)
+        error = (feedback_measurements - feedback_predicted).abs().pow(2).sum()
+
+        # Gradient descent step
+        error.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if do_plot and it % plot_per_its == 0:
+            plt.clf()
+            plt.subplot(1, 3, 1)
+            plot_feedback_fit(feedback_measurements, feedback_predicted, gray_values0, gray_values1)
+            plt.pause(0.01)
+
+        progress_bar.update()
+
+    if do_end_plot:
+        if do_plot:
+            plt.clf()
+        else:
+            plt.figure(figsize=(13, 4))
+        plt.subplot(1, 3, 1)
+
+        # TODO: plot LUT vs correct LUT
+        plot_feedback_fit(feedback_measurements, feedback_predicted, gray_values0, gray_values1)
+        plt.show()
+
+    return phase_response_per_gv
--- online_slm_calibration/online_calibrate_phase_response.py
+++ online_slm_calibration/online_calibrate_phase_response.py
@@ -2,9 +2,7 @@
 import os
 
 # External 3rd party
-import numpy as np
 import torch
-from tqdm import tqdm
 import matplotlib.pyplot as plt
 import h5py
 
@@ -30,55 +28,9 @@ filepath_measurements = os.path.join(localdata, 'harish_signal_feedback.mat')
 with h5py.File(filepath_measurements, "r") as f:
     file_dict = get_dict_from_hdf5(f)
 
-feedback_meas = torch.tensor(file_dict['feedback']).unsqueeze(0)
-gv1 = torch.tensor(file_dict['gv_row']).view(1, -1, 1)
-gv2 = torch.tensor(file_dict['gv_col']).view(1, 1, -1)
+feedback_meas = torch.tensor(file_dict['feedback'])
+gv0 = torch.tensor(file_dict['gv_row'])
+gv1 = torch.tensor(file_dict['gv_col'])
 
 lut_correct = import_lut(filepath_lut=filepath_lut)
 
-# Create init prediction
-
-
-# Initialize parameters and optimizer
-learning_rate = 3e-2
-params = [{'lr': learning_rate, 'params': [a, b, c]}]
-optimizer = torch.optim.Adam(params, lr=learning_rate, amsgrad=True)
-
-
-iterations = 30000
-progress_bar = tqdm(total=iterations)
-
-if do_plot:
-    plt.figure(figsize=(13, 4))
-
-for it in range(iterations):
-    feedback = predict_feedback(phase1, phase2, a, b, c, N, noise_level)
-
-    error = (feedback_meas - feedback).abs().pow(2).sum()
-
-    # Gradient descent step
-    error.backward()
-    optimizer.step()
-    optimizer.zero_grad()
-
-    if do_plot and it % plot_per_its == 0:
-        plt.clf()
-        plt.subplot(1, 3, 1)
-        plot_phase_curve(c, phase1, phase_lut_correct)
-        plot_feedback_fit(feedback_meas, feedback, phase1, phase2)
-        plt.pause(0.01)
-
-    progress_bar.update()
-
-if do_plot:
-    plt.clf()
-else:
-    plt.figure(figsize=(13, 4))
-plt.subplot(1, 3, 1)
-plot_phase_curve(c, phase1, phase_lut_correct)
-plot_feedback_fit(feedback_meas, feedback, phase1, phase2)
-plt.show()
-
-print(f'a: {a}')
-print(f'b: {b}')
-print(f'c: {c}')
