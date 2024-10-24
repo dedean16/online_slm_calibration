bc6cee0b5941eb3dbe1a474d51525fd83b6519ed
Daniel Cox <danielcox16@hotmail.com>
2024-10-24 14:52:25 +0200

    Import reference phase response measurement

--- online_slm_calibration/online_calibrate_phase_response.py
+++ online_slm_calibration/online_calibrate_phase_response.py
@@ -16,31 +16,34 @@ from directories import data_folder
 do_plot = True
 do_end_plot = True
 N = 2                           # Non-linearity factor. 1 = linear, 2 = 2PEF, 3 = 3PEF, etc., 0 = PMT is broken :)
-iterations = 1000
 
-
-filepath_lut = data_folder.joinpath('2023_08_inline_slm_calibration/LUT Files/corrected_2022_08_26 10-47-28.blt')
+filepath_ref = data_folder.joinpath('slm_reference_phase_response.mat')     # Reference phase response
 filepath_measurements = data_folder.joinpath('slm_calibration_signal_feedback.mat')
 
+# Import feedback measurements
 with h5py.File(filepath_measurements, "r") as f:
-    file_dict = get_dict_from_hdf5(f)
+    feedback_dict = get_dict_from_hdf5(f)
 
-feedback_meas = torch.tensor(file_dict['feedback'])
-gv0 = torch.tensor(file_dict['gv_row'] % 256, dtype=torch.int32)
-gv1 = torch.tensor(file_dict['gv_col'] % 256, dtype=torch.int32)
+feedback_meas = torch.tensor(feedback_dict['feedback'])
+gv0 = torch.tensor(feedback_dict['gv_row'] % 256, dtype=torch.int32)
+gv1 = torch.tensor(feedback_dict['gv_col'] % 256, dtype=torch.int32)
 
-lut_correct = import_lut(filepath_lut=filepath_lut)
+# Import reference phase response measurements
+with h5py.File(filepath_ref) as f:
+    ref_dict = get_dict_from_hdf5(f)
 
+# Learn phase response
 lr, phase_response_per_gv_fit, amplitude = learn_field(
-    gray_values0=gv0, gray_values1=gv1,measurements=feedback_meas, nonlinearity=N, learning_rate=0.05, iterations=1000,
+    gray_values0=gv0, gray_values1=gv1, measurements=feedback_meas, nonlinearity=N, learning_rate=0.05, iterations=100,
     do_plot=do_plot, do_end_plot=do_end_plot, plot_per_its=30)
 
 print(f'b = {amplitude.mean()}, lr = {lr} (1.0)')
 
 plt.figure()
 plt.subplot(2, 1, 1)
-# plt.plot(phase_response_per_gv_gt, color='C0', label='Ground truth')
-plt.plot(phase_response_per_gv_fit, '--', color='C1', label='Predicted')
+plt.errorbar(ref_dict['gray_values'][0], ref_dict['phase_mean'][0], yerr=ref_dict['phase_std'][0],
+             linestyle='--', color='#333333', label='Reference')
+plt.plot(phase_response_per_gv_fit, color='C0', label='Predicted')
 plt.xlabel('Gray value')
 plt.ylabel('Phase response')
 plt.legend()
