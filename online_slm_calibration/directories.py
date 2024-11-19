a761944017de76668d4fc6ab601ba0cb0785046d
Daniel Cox <danielcox16@hotmail.com>
2024-11-19 15:51:30 +0100

    Remove local data, put all data in data_folder

--- online_slm_calibration/directories.py
+++ online_slm_calibration/directories.py
@@ -4,8 +4,6 @@ This file defines file system paths to e.g. data folders.
 from pathlib import Path
 
 
-local_data = Path("C:/LocalData")
-
 data_folder = Path("c://git//online_slm_calibration//data")
 # data_folder = Path("//ad.utwente.nl/TNW/BMPI/Data/Daniel Cox/ExperimentalData/online_slm_calibration")
 # data_folder = Path("/home/dani/LocalData/online_slm_calibration")
