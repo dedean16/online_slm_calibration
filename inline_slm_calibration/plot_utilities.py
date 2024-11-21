9f8915e171ba6807076703162164a88df4a494ad
Daniel Cox <danielcox16@hotmail.com>
2024-11-21 17:29:14 +0100

    Remove difference plots

--- inline_slm_calibration/plot_utilities.py
+++ inline_slm_calibration/plot_utilities.py
@@ -5,40 +5,27 @@ from matplotlib import pyplot as plt
 
 def plot_results_ground_truth(gray_values, phase, amplitude, gray_values_ref, phase_ref, phase_ref_err, amplitude_ref):
 
-    plt.figure(figsize=(9, 8))
-    plt.subplots_adjust(left=0.1, right=0.96, hspace=0.35, wspace=0.35, top=0.95, bottom=0.08)
+    plt.figure(figsize=(9, 5))
+    plt.subplots_adjust(left=0.1, right=0.95, hspace=0.35, wspace=0.35, top=0.92, bottom=0.12)
 
-    plt.subplot(2, 2, 1)
+    plt.subplot(1, 2, 1)
     plt.errorbar(gray_values_ref, phase_ref, yerr=phase_ref_err, color='C0', label='Reference')      # plot phase with std error
-    plt.plot(gray_values, phase, '+', color='C1', label='Our fit')
+    plt.plot(gray_values, phase, '+', color='C1', label='Inline (ours)')
     plt.xlabel('Gray value')
     plt.ylabel('Phase')
     plt.title('a. Phase response')
     plt.legend()
 
-    plt.subplot(2, 2, 2)
+    plt.subplot(1, 2, 2)
     rel_amplitude_ref = amplitude_ref / amplitude_ref.mean()
     rel_amplitude = amplitude / amplitude.mean()
+    plt.plot(gray_values, rel_amplitude, '+', color='C1', label='Inline (ours)')
     plt.plot(rel_amplitude_ref, color='C0', label='Reference')
-    plt.plot(gray_values, rel_amplitude, '+', color='C1', label='Our fit')
     plt.xlabel('Gray value')
     plt.ylabel('Normalized amplitude')
     plt.ylim((0, 1.1 * np.maximum(rel_amplitude.max(), rel_amplitude_ref.max())))
     plt.title('b. Normalized amplitude response')
     plt.legend()
-
-    # With the current measurements, there are some deviations due to the actual gray values used
-    plt.subplot(2, 2, 3)
-    plt.plot(abs(phase - phase_ref), color='red')
-    plt.xlabel('Gray value')
-    plt.ylabel('Phase difference (rad)')
-    plt.title('c.')
-
-    plt.subplot(2, 2, 4)
-    plt.plot(abs(rel_amplitude - rel_amplitude_ref)[:-3], color='red')
-    plt.xlabel('Gray value')
-    plt.ylabel('Normalized amplitude difference')
-    plt.title('d.')
 
     plt.figure()
     plt.subplots_adjust(left=0.2, right=0.95, top=0.9, bottom=0.15)
