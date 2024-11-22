574610bb9fce9e3b4e23a425563676836f10a540
Daniel Cox <danielcox16@hotmail.com>
2024-11-22 16:06:38 +0100

    Update P-drive directory

--- inline_slm_calibration/directories.py
+++ inline_slm_calibration/directories.py
@@ -4,7 +4,7 @@ This file defines file system paths to e.g. data folders.
 from pathlib import Path
 
 
-data_folder = Path("c://git//online_slm_calibration//data")
-# data_folder = Path("//ad.utwente.nl/TNW/BMPI/Data/Daniel Cox/ExperimentalData/online_slm_calibration")
+# data_folder = Path("c://git//online_slm_calibration//data")
+data_folder = Path("//ad.utwente.nl/TNW/BMPI/Data/Daniel Cox/ExperimentalData/paper4-inline-slm-calibration")
 # data_folder = Path("/home/dani/LocalData/online_slm_calibration")
 
